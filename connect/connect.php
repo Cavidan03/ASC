cd914cd7dfe91c38ea0c7a4d4e1beb7f193600dd
Cavidan03 <aa4060918@gmail.com>
2024-12-16 13:16:16 +0400

    Changes the Port

--- connect/connect.php
+++ connect/connect.php
@@ -1,7 +1,7 @@
 <?php
 
   try{
-    $db=new PDO("mysql:host=localhost;dbname=asm;charset-utf8mb4","root","");
+    $db=new PDO("mysql:host=localhost;port=4306;dbname=asm;charset-utf8mb4","root","");
    $db->exec("SET NAMES 'utf8mb4'");
    $db->exec("SET CHARSET 'utf8mb4'");
 
